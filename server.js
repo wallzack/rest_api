53c0a89d81229ca6cf58b138016984e31689d0fb
wallzack <vulcheek@gmail.com>
2020-04-05 18:33:11 +0200

    22.5 added socket.io

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const uuidv4 = require('uuid/v4');
 const path = require('path');
+const socket = require('socket.io');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -27,6 +28,12 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Oops... Not found...' });
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New socket!');
 });
