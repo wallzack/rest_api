c98c46f90a284839bd45b8190f7cf3c312d07fe9
wallzack <vulcheek@gmail.com>
2020-04-12 23:02:02 +0200

    23.5 added NewWaveDB do server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const uuidv4 = require('uuid/v4');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -33,7 +34,16 @@ app.get('*', (req, res) => {
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
-})
+});
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {userNewUrlParser: true, userUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', err => console.log('Error' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000');
