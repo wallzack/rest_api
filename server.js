143a1e795fc87b2439f620c8fed1baa6f8364fab
wallzack <vulcheek@gmail.com>
2020-04-22 18:02:28 +0200

    24.5 sth wrong with the db, workshops don't show up

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-mongoose.connect('mongodb+srv://user-mkw:k913aydynjwSXsYl@cluster0-ltnvo.mongodb.net/festivalDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://wallzack:47BFKGK4SN0x5gC2@cluster0-ksbbn.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
