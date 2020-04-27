e9a9026e971d2bd717ec391678b59ab01a6edb72
wallzack <vulcheek@gmail.com>
2020-04-27 19:16:22 +0200

    25.1 added helmet

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const uuidv4 = require('uuid/v4');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname + '/client/build')));
 
 app.use(cors());
+
+app.use(helmet());
 
 app.use((req, res, next) => {
   req.io = io;
